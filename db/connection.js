9f80de42d9267b7b36b2eeb6d1271a543934d631
deon <mkandaniel@gmail.com>
2022-07-13 09:16:44 -0700

    mongoose connection

--- db/connection.js
+++ db/connection.js
@@ -1,18 +1,23 @@
 const MongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose')
 
 const uri = 'mongodb+srv://destinationuser:page1destination@cluster0.q5oz7.mongodb.net/?retryWrites=true&w=majority'
 
-let db = null;
+mongoose.connect(uri, { useNewUrlParser: true }, { useUnifiedTopology: true }, () => {
+	console.log('MongoDB Connected as Local Database');
+})
 
-module.exports = {
-    connectToServer: (callback) => {
-        MongoClient.connect(uri, {useNewUrlParser: true}, function(err, client){
-            db = client.db('destination')
-            console.log('we connected')
-            return callback(err)
-        })
-    },
-    getDb: () => {
-        return db
-    }
-}
+// let db = null;
+
+// module.exports = {
+//     connectToServer: (callback) => {
+//         MongoClient.connect(uri, {useNewUrlParser: true}, function(err, client){
+//             db = client.db('destination')
+//             console.log('we connected')
+//             return callback(err)
+//         })
+//     },
+//     getDb: () => {
+//         return db
+//     }
+// }
