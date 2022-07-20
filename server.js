cf7cebd1eef9e5cd7cb0b88456f23939495e8d83
deon <mkandaniel@gmail.com>
2022-07-20 12:19:38 -0700

    test

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const { requiresAuth } = require('express-openid-connect');
 // app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
@@ -29,7 +28,7 @@ const config = {
 app.use(auth(config));
 
 // req.isAuthenticated is provided from the auth router
-app.get('/destinations', (req, res) => {
+app.get('/', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
@@ -37,12 +36,9 @@ app.get('/profile', requiresAuth(), (req, res) => {
   res.send(JSON.stringify(req.oidc.user));
 });
 
-const connection = require('./db/connection');
-connection.connectToServer(function(err, client) {
-    if (err) {
-        console.log(err)
-    }
-})
+// app.get('/details', (req, res) => {
+//     res.render('/views/index.ejs')
+// })
 
 const destinationController = require('./controllers/destinationController')
 app.use('/', destinationController)
